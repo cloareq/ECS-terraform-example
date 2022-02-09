b1e09c0a10273f858dd09474cfef2a646612c72f
cloareq <quentincperso@gmail.com>
2022-02-09 16:16:07 +0100

    Fix staging vpc subnets

--- staging/variables.tf
+++ staging/variables.tf
@@ -2,6 +2,6 @@ locals {
   aws_region         = "eu-west-1"
   availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   environment        = "staging"
-  private_subnets    = ["10.10.64.0/18", "10.10.128.0/18", "10.10.192.0/18"]
-  public_subnets     = ["10.11.64.0/18", "10.11.128.0/18", "10.11.192.0/18"]
+  private_subnets    = ["10.10.1.0/24", "10.10.2.0/24", "10.10.3.0/24"]
+  public_subnets     = ["10.10.10.0/24", "10.10.11.0/24", "10.10.12.0/24"]
 }
